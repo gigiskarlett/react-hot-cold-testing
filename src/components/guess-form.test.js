b97a07c3c5050c6d9a1dbbd68e113fed6eb3d766
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-02-07 10:21:40 -0800

    smoke test for guess-form

--- src/components/guess-form.test.js
+++ src/components/guess-form.test.js
@@ -1,5 +1,10 @@
-import React from 'react';
-import { shallow, mount } from 'enzyme';
+import React from "react";
+import { shallow, mount } from "enzyme";
 
-import GuessForm from './guess-form';
+import GuessForm from "./guess-form";
 
+describe("<GuessForm />", () => {
+  it("Renders without crashing", () => {
+    shallow(<GuessForm />);
+  });
+});
