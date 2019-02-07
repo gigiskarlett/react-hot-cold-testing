800530174bf38eb61e99f551eea8e640a58a3248
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-02-07 14:53:52 -0800

    gives auralStatu s

--- src/components/aural-status.test.js
+++ src/components/aural-status.test.js
@@ -7,4 +7,10 @@ describe("<AuralStatus />", () => {
     it("renders without crashing", () => {
        shallow(<AuralStatus />)
     })
+
+    it("gives auralStatus", () => {
+        const auralStatus = "warm";
+        const wrapper= shallow(<AuralStatus auralStatus={auralStatus} />);
+        expect(wrapper.contains(auralStatus)).toEqual(true)
+    })
 })
