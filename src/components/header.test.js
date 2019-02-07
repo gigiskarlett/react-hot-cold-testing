6aa77d5e7422e5693b5eeb00249fd06952dcc47d
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-02-07 11:22:17 -0800

    created smoke test

--- src/components/header.test.js
+++ src/components/header.test.js
@@ -3,3 +3,8 @@ import { shallow } from 'enzyme';
 
 import Header from './header';
 
+describe("<Header />", () => {
+    it("renders without crashing", () => {
+        shallow(<Header />)
+    })
+})
