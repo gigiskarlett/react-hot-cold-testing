503ba7326d785f3d795d9589b1b5e332425e3276
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-02-07 10:34:34 -0800

    created smoke test

--- src/components/aural-status.test.js
+++ src/components/aural-status.test.js
@@ -2,3 +2,9 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import AuralStatus from './aural-status';
+
+describe("<AuralStatus />", () => {
+    it("renders without crashing", () => {
+       shallow(<AuralStatus />)
+    })
+})
