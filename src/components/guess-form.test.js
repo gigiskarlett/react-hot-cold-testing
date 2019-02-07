f43ad3b2239ac28c14b01628a38f93c869f87e42
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-02-07 13:37:54 -0800

    created test for submit

--- src/components/guess-form.test.js
+++ src/components/guess-form.test.js
@@ -7,4 +7,14 @@ describe("<GuessForm />", () => {
   it("Renders without crashing", () => {
     shallow(<GuessForm />);
   });
+
+  it("calls onMakeGuess when form submits", () => {
+    const callback = jest.fn()
+    const wrapper= mount(<GuessForm onMakeGuess={callback} />);
+    const button = wrapper.find(".button");
+    button.simulate("submit", {
+      preventDefault() {}
+    });
+    expect(callback).toHaveBeenCalled();
+  })
 });
