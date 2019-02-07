90ceed6893494a4abdbe9ac68ff6bfbed772d1d2
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-02-07 11:36:56 -0800

    test for new game callback

--- src/components/top-nav.test.js
+++ src/components/top-nav.test.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import { shallow } from 'enzyme';
+import React from "react";
+import { shallow } from "enzyme";
 
-import TopNav from './top-nav';
+import TopNav from "./top-nav";
 
 describe("<TopNav />", () => {
-    it("renders without crashing", () => {
-        shallow(<TopNav />)
-    })
-})
+  it("renders without crashing", () => {
+    shallow(<TopNav />);
+  });
+
+  it("Should call onNewGame when new game is clicked", () => {
+    const callback = jest.fn();
+    const wrapper = shallow(<TopNav onRestartGame={callback} />);
+    const link = wrapper.find(".new");
+    link.simulate("click", {
+      preventDefault() {}
+    });
+    expect(callback).toHaveBeenCalled();
+  });
+});
