791248ad03ecf631bef3fe5e0edc27ecd7be4504
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-02-07 14:13:16 -0800

    counts passed guesses

--- src/components/guess-count.test.js
+++ src/components/guess-count.test.js
@@ -7,4 +7,11 @@ describe("<GuessCount />", () => {
   it("renders without crashing", () => {
       shallow(<GuessCount />)
   });
+
+  it("should count how many guesses user has", () => {
+    const values= [9];
+    const wrapper= shallow(<GuessCount guesses={values} />);
+    const count= wrapper.find("h2");
+    expect(count.length).toEqual(values.length)
+  })
 });
