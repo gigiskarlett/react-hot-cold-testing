43daf9f4c6069963e6d4970b8878b859bb894c2f
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-02-07 14:46:59 -0800

    gives feedback to player

--- src/components/feedback.test.js
+++ src/components/feedback.test.js
@@ -7,4 +7,10 @@ describe("<Feedback />", () => {
     it("renders without crashing", () => {
         shallow(<Feedback />)
     })
+
+    it("gives feedback to player", () => {
+        const feedback = "you're hot"
+        const wrapper= shallow(<Feedback feedback={feedback} />);
+        expect(wrapper.contains(feedback).toEqual(true);
+    })
 })
